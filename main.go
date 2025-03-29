f8d391dab4e583bf6a8208e3cd644ef88c234f2b
Kavisha Mathur <kavisha.mathur1@tesco.com>
2025-03-29 08:27:58 +0530

    Added workflows and stash, checkout, rebase

--- main.go
+++ main.go
@@ -4,43 +4,46 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 )
 
-// runCommand executes a shell command in the given directory
-func runCommand(dir, cmd string, args ...string) {
-	command := exec.Command(cmd, args...)
-	command.Dir = dir
-	command.Stdout = os.Stdout
-	command.Stderr = os.Stderr
-	if err := command.Run(); err != nil {
-		fmt.Println("Error:", err)
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: git-auto <project-path> <command> [arguments]")
+		return
+	}
+
+	projectPath := os.Args[1]
+	command := os.Args[2]
+	args := os.Args[3:]
+
+	err := os.Chdir(projectPath)
+	if err != nil {
+		fmt.Println("Error: Unable to navigate to", projectPath)
+		return
+	}
+
+	switch command {
+	case "commit":
+		runCommand("git", "add", ".")
+		runCommand("git", "commit", "-m", args[0])
+		runCommand("git", "pull", "--rebase")
+		runCommand("git", "push")
+	case "stash":
+		runCommand("git", "stash")
+	case "checkout":
+		runCommand("git", "checkout", args[0])
+	case "rebase":
+		runCommand("git", "rebase", args[0])
+	default:
+		fmt.Println("Invalid command. Supported commands: commit, stash, checkout, rebase")
 	}
 }
 
-func main() {
-	// Ensure a directory is provided
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: git-auto <project-path> [commit-message]")
-		return
-	}
-
-	projectPath, err := filepath.Abs(os.Args[1]) // Get absolute path of project
+func runCommand(name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		fmt.Println("Invalid path:", err)
-		return
+		fmt.Println("Error:", err)
 	}
-
-	commitMessage := "Automated commit"
-	if len(os.Args) > 2 {
-		commitMessage = os.Args[2]
-	}
-
-	fmt.Println("Running Git automation in:", projectPath)
-
-	// Run Git commands in the provided directory
-	runCommand(projectPath, "git", "pull")
-	runCommand(projectPath, "git", "add", ".")
-	runCommand(projectPath, "git", "commit", "-m", commitMessage)
-	runCommand(projectPath, "git", "push")
+	fmt.Println(string(output))
 }
